0858d6233153da78f9987d4ba4fabd9facd01403
Khush Ramani <76166621+LynX-gh@users.noreply.github.com>
2023-12-18 21:35:15 +0530

    Part 1-4
    Signed-off-by: Khush Ramani <76166621+LynX-gh@users.noreply.github.com>

---
+++ MS_CSharp_Tutorial/For.cs
@@ -0,0 +1,11 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
---
+++ MS_CSharp_Tutorial/SortReverse.cs
@@ -0,0 +1,16 @@
+﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
